1eb98b45deafb43c00b327864259c6cb79395425
vitalie stinca <vitalicstinca123@gmail.com>
2024-08-29 16:41:20 +0300

    feat: Write function for loading test images

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import usefull packages
+
 from utils.logger_setup import setup_logger
 import logging
 import matplotlib.pyplot as plt
@@ -7,7 +8,19 @@ import numpy as np
 import cv2
 import os
 import glob
-from moviepy.editor import VideoFileClip
+# from moviepy.editor import VideoFileClip
+
+# @matplotlib inline
 
 app_logger = setup_logger("app_logger", logging.INFO)
 
+def list_images(images, cols = 2, rows = 5, cmap = None):
+    plt.figure(figsize=(10, 11))
+    for i, image in enumerate(images):
+        plt.subplot(rows, cols, i+1)
+        cmap = 'gray' if len(image.shape) == 2 else cmap
+        plt.imshow(image, cmap=cmap)
+        plt.xticks([])
+        plt.yticks([])
+    plt.tight_layout(pad=0, h_pad=0, w_pad=0)
+    plt.show()
